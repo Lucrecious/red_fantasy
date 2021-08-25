7823fe66a5dd5783cefa6ef2a23d7e7c88537021
Lucrecious <luca.duran@hotmail.com>
2021-08-24 21:23:04 -0400

    Fix final boss not stopping to bleed

--- project/src/particles/bleed.gd
+++ project/src/particles/bleed.gd
@@ -6,10 +6,11 @@ onready var _body := NodE.get_ancestor_with_error(self, KinematicBody2D) as Kine
 onready var _health := NodE.get_child(_body, Component_Health) as Component_Health
 
 func _ready():
-	_health.connect('damaged', self, '_on_health_damaged')
-	_on_health_damaged(0)
+	_health.connect('damaged', self, '_on_health_changed')
+	_health.connect('increased', self, '_on_health_changed')
+	_on_health_changed(0)
 
-func _on_health_damaged(_amount: int) -> void:
+func _on_health_changed(_amount: int) -> void:
 	var current := _health.current
 	if current == 0:
 		emitting = false
